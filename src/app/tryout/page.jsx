7217a5da3e49e46d185fd3a36b7042faeced894f
RodriigoCode <rodriigocode2004@gmail.com>
2024-07-26 20:36:28 +0100

    first commit

--- src/app/tryout/page.jsx
+++ src/app/tryout/page.jsx
@@ -12,18 +12,28 @@ const TryoutPage = () => {
         loop
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
       />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-lg shadow-lg text-center max-w-sm">
-        <h1 className="text-2xl font-semibold mb-4">Tryout | Form</h1>
-        <p className="text-base mb-6">
-          Join Discord and Form | Tryout Season starting soon
-        </p>
-        <div className="flex justify-center gap-5">
-          <a href="https://discord.gg/7hnmg3tq" target="_blank" rel="noopener noreferrer" className="text-black">
-            <FaDiscord size={60} />
-          </a>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSfP4rRakC_RNfxfacwlxFGj3spKNf5gQI7XfXTQoZL5N1o8_g/viewform" target="_blank" rel="noopener noreferrer" className="text-black">
-            <FaSheetPlastic size={50} />
-          </a>
+      <div className="flex flex-col items-center justify-center h-full gap-10">
+        <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-sm">
+          <h1 className="text-2xl font-semibold mb-4">Tryout | Form</h1>
+          <p className="text-base mb-6">
+            Join Discord and Form | Tryout Season starting soon
+          </p>
+          <div className="flex justify-center gap-5">
+            <a href="https://discord.gg/7hnmg3tq" target="_blank" rel="noopener noreferrer" className="text-black">
+              <FaDiscord size={60} />
+            </a>
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSfP4rRakC_RNfxfacwlxFGj3spKNf5gQI7XfXTQoZL5N1o8_g/viewform" target="_blank" rel="noopener noreferrer" className="text-black">
+              <FaSheetPlastic size={50} />
+            </a>
+          </div>
+        </div>
+        <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-sm">
+          <h1 className="text-2xl font-semibold mb-4">Como entrar na Peneira</h1>
+          <video
+            src="/tutorial.mp4" // Use o caminho relativo do diretório `public`
+            controls
+            className="w-full h-auto rounded-lg"
+          />
         </div>
       </div>
     </div>
